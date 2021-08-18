91b15f7788cbf5a09481f4346c7c5f4d2d948518
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-18 10:00:42 -0300

    Updates

---
+++ precache-manifest.468e12b318902f3c338bf1240161c1d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77721035eec6bd84ab62c4d8add9989",
+    "url": "/project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "257e61cfa77033dc65fe",
+    "url": "/project-trivia-react-redux/static/css/main.736f3f80.chunk.css"
+  },
+  {
+    "revision": "f93a7f22d027f277d24a",
+    "url": "/project-trivia-react-redux/static/js/2.5880a0c6.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/project-trivia-react-redux/static/js/2.5880a0c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "257e61cfa77033dc65fe",
+    "url": "/project-trivia-react-redux/static/js/main.eededc42.chunk.js"
+  },
+  {
+    "revision": "5f1245b44c44d3da06a6",
+    "url": "/project-trivia-react-redux/static/js/runtime-main.2ea505b7.js"
+  },
+  {
+    "revision": "12ac286a10583e067e777cab2381ecef",
+    "url": "/project-trivia-react-redux/static/media/arrow.12ac286a.png"
+  },
+  {
+    "revision": "091b40f3a7ddf83760b656937891e051",
+    "url": "/project-trivia-react-redux/static/media/questionMark.091b40f3.png"
+  },
+  {
+    "revision": "96f64e3dfe34f74a6dc5b3e505ae8eef",
+    "url": "/project-trivia-react-redux/static/media/questions.96f64e3d.png"
+  },
+  {
+    "revision": "a1c4da76ae005f91cca2a5be44294168",
+    "url": "/project-trivia-react-redux/static/media/ranking.a1c4da76.png"
+  },
+  {
+    "revision": "bbf3896b55f240bf9b08b628abae2ad7",
+    "url": "/project-trivia-react-redux/static/media/stonewall2.bbf3896b.jpg"
+  },
+  {
+    "revision": "8ee717d1ca61ba9b6f25d2c0d3d5dce3",
+    "url": "/project-trivia-react-redux/static/media/trivia.8ee717d1.png"
+  }
+]);
